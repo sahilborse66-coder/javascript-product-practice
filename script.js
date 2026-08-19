3964b3a28faf7c8131b25612e8fb2ea8616b5a66
Sahil Borse <sahilborse66@gmail.com>
2026-08-19 23:52:37 +0530

    Added JavaScript practice

--- script.js
+++ script.js
@@ -1,270 +1,164 @@
-// let products = [
-//     { id: 1, name: "Laptop", price: 55000, quantity: 1 },
-//     { id: 2, name: "Mouse", price: 800, quantity: 2 },
-//     { id: 3, name: "Keyboard", price: 1500, quantity: 1 }
-// ];
+// for(let count=1; count<=10; count++ ){
+//     console.log("Sahil Java");
+// }
 
-// Create functions for:
-//
-// 1. Add product
-// 2. Remove product
-// 3. Increase quantity
-// 4. Decrease quantity
-// 5. Calculate total
-// 6. Apply discount
-// 7. Calculate GST
-// 8. Print final bill
+// console.log ("loop has ended");
 
+// let sum = 0;
+// for (let i = 1; i <= 5; i++){
+// sum = sum + i;
+// }
+
+// console.log("sum = " , sum);
+// console.log ("loop has ended");
+
+
+// for (let i=1; i >=0; i++)
 // {
-//     id: 1,
-//     name: "Sahil",
-//     age: 22,
-//     marks: {
-//         java: 85,
-//         javascript: 90,
-//         sql: 78
+//     console.log("i=", i);
+// }  
+
+// let i = 5;
+// while(i<=10){
+//     console.log("sahil borse");
+//     i++;
+// }
+
+// let i = 1;
+// do{
+//     console.log("i=" , i);
+//     i++;
+// } while (i <= 10);
+
+
+// var age =25;
+
+// if(true){
+//     console.log(age);
+// }
+
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// solve();
+
+// let marks = 345424255522555345363433675867976856763553235325534545867876967754234142403506457939460356;
+// console.log(marks);
+
+// let a = 10;
+// let b =5;
+
+// console.log(a+b);
+
+// let a = 10;
+// let b =5;
+
+// console.log(a-b);
+
+
+// let a = 10;
+// let b =5;
+
+// console.log(a*b);
+
+// let a = 2;
+// let b =5;
+
+// console.log(a**b);
+
+// console.log(10 > 5);
+// console.log(10 < 5);
+
+
+// let ans = (false && true && true);
+
+
+// let ans = (true || false || false );
+
+// let ans = !(true);
+
+// console.log (false || 7 || 11 || 18);
+
+// console.log( 2 ^ 1 );
+
+
+// console.log(30 >> 10);
+
+
+// let age = 19;
+
+// if (age >= 18){
+//     console.log('can vote');
+
+// }
+// else {
+//     console.log('cannot vote');
+// }
+
+// let number = 5;
+
+// if (number == 1){
+//     console.log('A');
+// }
+
+// else if (number == 2){
+//     console.log('B');
+// }
+
+// else if( number == 3){
+//     console.log('C');
+// }
+
+// else if (number == 4){
+//     console.log('D');
+// }
+
+// else if(number == 5){
+//     console.log('E');
+// }
+
+// else{
+//     console.log('F');
+// }
+
+
+// let num = 3;
+
+// switch(num){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     case 4: console.log('D');
+//     break;
+//     default : console.log('F');
+// }
+
+
+// for(let i=5; i>0; i--){
+//     console.log(i);
+// }
+
+
+// for (let i=1; i<=5; i++){
+//     if(i==4){
+//         break;
+//     }
+//     else
+//     {
+//         console.log(i);
 //     }
 // }
 
-// let products = [
-//     {
-//         id: 1,
-//         name: "iPhone 15",
-//         category: "Mobile",
-//         price: 65000,
-//         rating: 4.5,
-//         stock: 10
-//     },
-//     {
-//         id: 2,
-//         name: "HP Laptop",
-//         category: "Laptop",
-//         price: 55000,
-//         rating: 4.2,
-//         stock: 5
-//     },
-//     {
-//         id: 3,
-//         name: "Sony Headphones",
-//         category: "Audio",
-//         price: 5000,
-//         rating: 4.4,
-//         stock: 20
-//     }
-// ];
-
-// let employees = [
-//     {
-//         id: 101,
-//         name: "Sahil",
-//         age: 22,
-//         department: "IT",
-//         salary: 45000,
-//         experience: 2
-//     },
-//     {
-//         id: 102,
-//         name: "Rahul",
-//         age: 25,
-//         department: "HR",
-//         salary: 40000,
-//         experience: 3
-//     },
-//     {
-//         id: 103,
-//         name: "Amit",
-//         age: 28,
-//         department: "IT",
-//         salary: 65000,
-//         experience: 5
-//     },
-//     {
-//         id: 104,
-//         name: "Priya",
-//         age: 24,
-//         department: "Finance",
-//         salary: 50000,
-//         experience: 2
-//     }
-// ];
 
 
-
-
-// addEmployee({
-//     id: 105,
-//     name: "Neha",
-//     age: 26,
-//     department: "IT",
-//     salary: 55000,
-//     experience: 4
-// });
-
-
-// let accounts = [
-//     {
-//         accountNumber: 1001,
-//         name: "Sahil",
-//         pin: 1234,
-//         balance: 50000,
-//         transactions: []
-//     },
-//     {
-//         accountNumber: 1002,
-//         name: "Rahul",
-//         pin: 5678,
-//         balance: 30000,
-//         transactions: []
-//     }
-// ];
-
-
-// createAccount(
-//     "Amit",
-//     9999,
-//     25000
-// );
-
-// let products = [
-//     {
-//         id: 1,
-//         name: "iPhone 15",
-//         category: "Mobile",
-//         price: 65000,
-//         stock: 10,
-//         rating: 4.5
-//     },
-//     {
-//         id: 2,
-//         name: "HP Laptop",
-//         category: "Laptop",
-//         price: 55000,
-//         stock: 7,
-//         rating: 4.2
-//     },
-//     {
-//         id: 3,
-//         name: "Sony Headphones",
-//         category: "Audio",
-//         price: 5000,
-//         stock: 15,
-//         rating: 4.4
-//     },
-//     {
-//         id: 4,
-//         name: "Samsung Monitor",
-//         category: "Monitor",
-//         price: 18000,
-//         stock: 5,
-//         rating: 4.1
-//     }
-// ];
-
-// let cart = [];
-
-// let orders = [];
-
-
-
-let products = [
-    {
-        id: 1,
-        name: "Nike Running Shoes",
-        category: "Shoes",
-        price: 2999,
-        rating: 4.5,
-        image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
-    },
-
-    {
-        id: 2,
-        name: "Smart Watch",
-        category: "Watch",
-        price: 4999,
-        rating: 4.3,
-        image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
-    },
-
-    {
-        id: 3,
-        name: "iPhone",
-        category: "Mobile",
-        price: 69999,
-        rating: 4.7,
-        image: "./images/iphone.jpg"
-    },
-
-    {
-        id: 4,
-        name: "Laptop",
-        category: "Laptop",
-        price: 55999,
-        rating: 4.4,
-        image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
-    },
-
-    {
-        id: 5,
-        name: "Casual Shoes",
-        category: "Shoes",
-        price: 1999,
-        rating: 4.2,
-        image: "https://images.unsplash.com/photo-1549298916-b41d501d3772"
-    },
-
-    {
-        id: 6,
-        name: "Luxury Watch",
-        category: "Watch",
-        price: 8999,
-        rating: 4.6,
-        image: "https://images.unsplash.com/photo-1524805444758-089113d48a6d"
-    }
-];
-
-let productContainer = document.getElementById("productContainer");
-
-function displayProducts() {
-
-    productContainer.innerHTML = "";
-
-    products.forEach(function(product) {
-
-        productContainer.innerHTML += `
-        
-            <div class="product-card">
-
-                <img src="${product.image}" alt="${product.name}">
-
-                <h2>${product.name}</h2>
-
-                <p>Category: ${product.category}</p>
-
-                <h3>₹${product.price}</h3>
-
-                <p>⭐ ${product.rating}</p>
-
-                <button onclick="addToCart(${product.id})">
-                    Add to Cart
-                </button>
-
-            </div>
-
-        `;
-    });
+for (let i=1; i<=4; i++){
+    if(i == 3)
+        continue;
+    else
+        console.log(i);
 }
-
-function addToCart(productId) {
-
-    let selectedProduct = products.find(function(product) {
-
-        return product.id === productId;
-
-    });
-
-    console.log("Product Added:", selectedProduct.name);
-
-    alert(selectedProduct.name + " Added To Cart!");
-}
-
-displayProducts();
